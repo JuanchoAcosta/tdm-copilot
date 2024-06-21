f023e0c5d60697838caf549e85e501e1b7cb3b1a
Juan Acosta <juan.acosta@kilimoagtech.com>
2024-06-21 20:31:46 -0300

    Added player

--- front/models.py
+++ front/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Player(models.Model):
+    name = models.CharField(max_length=100)
+    lastname = models.CharField(max_length=100)
+    age = models.IntegerField()
+    tmt_id = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.lastname}, {self.name}"
